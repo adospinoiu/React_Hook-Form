c89d5ba3860d9a1149d1f2b47b310034a220b8d7
adospinoiu <alex.dospinoiu@gmail.com>
2022-10-28 21:11:10 -0700

    Updated App.js file

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import "./styles.css";
 
 export default function App() {
+    // This comes directly from the react-hook-form library
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm()
+
+    const onSubmit = (data) => {
+        console.log(data)
+    }
+
     return (
         <div className="App">
-            <form>
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="form-control">
                     <label>Email</label>
                     <input
                         type="text"
-                        name="email" />
+                        name="email" {...register("email")} />
                 </div>
                 <div className="form-control">
                     <label>Password</label>
                     <input
                         type="text"
-                        name="password" />
+                        name="password" {...register("password")}/>
                 </div>
                 <div className="form-control">
                     <label></label>
